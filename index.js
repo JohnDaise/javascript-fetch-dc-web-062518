d8ff456adad35ca499a5fad27ce301b112c16bab
JohnDaise <jdaise@gmail.com>
2018-08-10 13:59:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const app = "I don't do much.";
+
+const token = 'ba73683bd553098c85bf2261e3ee625de587d00b'
+fetch('https://api.github.com/johndaise/repos').
+  then(res => res.json()).
+  then(json => console.log(json));
